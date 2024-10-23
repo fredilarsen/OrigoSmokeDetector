d09bba7b6fb55b10a3baef157c2cce83b5fe2217
Fred Larsen <fredilarsen@gmail.com>
2024-10-23 22:04:00 +0200

    Update UserValues.h
    Corrected typo in define DEBUG_PRINT_PARTIAL_SIGNALS

--- examples/OrigoMqtt/UserValues.h
+++ examples/OrigoMqtt/UserValues.h
@@ -2,9 +2,9 @@
 
 // Whether to print debug information to Serial. Typical use is to define the two on top, run it to scan and find
 // the SEQUENCE_HIGHBITS and SEQUENCE_LOWBITS to insert fiurther down, then undefine the debug prints when all working.
-#define DEBUG_PRINT                  // Print basic debug information (must be defined to define the other 2 below)
-#define DEBUG_PRINT_SIGNAL           // Whether to print complete signals and sequence bits (SCAN for bits)
-//#define DEBUG_PRINT_PARTIAL_SIGNAL // Whether to also print incomplete signals over a certain length.
+#define DEBUG_PRINT                   // Print basic debug information (must be defined to define the other 2 below)
+#define DEBUG_PRINT_SIGNAL            // Whether to print complete signals and sequence bits (SCAN for bits)
+//#define DEBUG_PRINT_PARTIAL_SIGNALS // Whether to also print incomplete signals over a certain length.
 
 // Note that this sequence is set by the main unit, and will change if it is reset.
 // Do a SCAN operation to find them, then configure them here.
